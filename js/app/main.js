fed4c3adc4fb7b5758deaf177b70a0c9cb4fd335
Hakall <abouskine@gmail.com>
2016-09-23 16:41:43 +0200

    manager and admin dashboard

--- js/app/main.js
+++ js/app/main.js
@@ -5,10 +5,13 @@
         $('.collapsible').collapsible({
             accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
         });
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 /** Vue.JS **/
+
+/** User **/
 var PushMethod = Vue.extend({
     props: {
         'user': Object,
@@ -91,7 +94,176 @@ var UserDashboard = Vue.extend({
                     if (data.code == "Ok") {
                         event.target.checked = true;
                         this.user.methods.push.active = true;
-                        console.log(data);
+                        this.user.methods.push.device.manufacturer = data.device.manufacturer;
+                        this.user.methods.push.device.model = data.device.model;
+                        this.user.methods.push.device.platform = data.device.platform;
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    event.target.checked = false;
+                    this.user.methods.push.active = false;
+                    console.error("/data/push-infos.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        askPushActivation: function(event) {
+            console.log("askPushActivation");
+            event.target.checked = false;
+            this.user.methods.push.active = false;
+            //ajax
+            $.ajax({
+                url: "/data/push-activation.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code == "Ok") {
+                        this.user.methods.push.activationCode = data.activationCode;
+                        this.user.methods.push.qrCode = data.qrCode;
+                        this.user.methods.push.api_url = data.api_url;
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error("/data/push-activation.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        activateBypass: function(event) {
+            this.user.methods.bypass.active = true;
+            event.target.checked = true;
+            this.generateBypass(function() {
+                this.user.methods.bypass.active = false;
+                event.target.checked = false;
+            })
+        },
+        activateTotp: function(event) {
+            this.user.methods.totp.active = true;
+            event.target.checked = true;
+            this.generateTotp(function() {
+                this.user.methods.totp.active = false;
+                event.target.checked = false;
+            })
+        },
+        activateRandomCode: function(event) {
+            this.user.methods.random_code.active = true;
+            event.target.checked = true;
+            $.ajax({
+                url: "/data/activate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") {
+                        this.user.methods.random_code.active = false;
+                        event.target.checked = false;
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    this.user.methods.random_code.active = false;
+                    event.target.checked = false;
+                    console.error("/data/activate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        deactivate: function(event) {
+            console.log("deactivate " + event.target.name);
+            event.target.checked = false;
+            this.user.methods[event.target.name].active = false;
+            $.ajax({
+                url: "/data/deactivate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") this.user.methods[event.target.name].active = true;
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    this.user.methods[event.target.name].active = true;
+                    event.target.checked = true;
+                    console.error("/data/deactivate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        generateBypass: function(onError) {
+            $.ajax({
+                url: "/data/bypass-secret.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code == "Ok") this.user.methods.bypass.codes = data.codes;
+                    else if (typeof(onError) === "function") onError();
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    if (typeof(onError) === "function") onError();
+                    console.error("/data/bypass-secret.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        generateTotp: function(onError) {
+            $.ajax({
+                url: "/data/totp-secret.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code == "Ok") {
+                        this.user.methods.totp.message = data.message;
+                        this.user.methods.totp.qrCode = data.qrCode;
+                        this.user.methods.totp.uid = data.uid;
+                    } else if (typeof(onError) === "function") onError();
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    if (typeof(onError) === "function") onError();
+                    console.error("/data/totp-secret.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+    }
+});
+
+/** Manager **/
+var UserView = Vue.extend({
+    props: {
+        'user': Object,
+        'methods': Object,
+        'messages': Object
+    },
+    components: {
+        "push": PushMethod,
+        "totp": TotpMethod,
+        "bypass": BypassMethod,
+        "random_code": RandomCodeMethod
+    },
+    template: '#user-view',
+    methods: {
+        activate: function(event) {
+            switch (event.target.name) {
+                case 'push':
+                    this.askPushActivation(event);
+                    break;
+                case 'bypass':
+                    this.activateBypass(event);
+                    break;
+                case 'random_code':
+                    this.activateRandomCode(event);
+                    break;
+                case 'totp':
+                    this.activateTotp(event);
+                    break;
+                default:
+                    /** **/
+                    event.target.checked = true;
+                    this.user.methods[event.target.name].active = true;
+                    break;
+            }
+        },
+        activatePush: function(event) {
+            console.log("activatePush");
+            //ajax
+            $.ajax({
+                url: "/data/push-infos.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code == "Ok") {
+                        event.target.checked = true;
+                        this.user.methods.push.active = true;
                         this.user.methods.push.device.manufacturer = data.device.manufacturer;
                         this.user.methods.push.device.model = data.device.model;
                         this.user.methods.push.device.platform = data.device.platform;
@@ -218,20 +390,172 @@ var UserDashboard = Vue.extend({
 var ManagerDashboard = Vue.extend({
     props: {
         'methods': Object,
-        'messages': Object,
-        'uids': Array,
+        'messages': Object
+    },
+    components: {
+        "user-view": UserView
+    },
+    data: function() {
+        return {
+            suggestions: [],
+            user: {
+                uid: String,
+                methods: Object,
+                transports: Object
+            },
+            focus: false,
+            uids: Array,
+        }
+    },
+    created: function() {
+        this.getUsers();
+    },
+    methods: {
+        suggest: function(event) {
+            this.suggestions = [];
+            if (event.target.value !== "") {
+                for (uid in this.uids) {
+                    if (this.uids[uid].includes(event.target.value)) this.suggestions.push(this.uids[uid]);
+                }
+                this.focus = true;
+            }
+        },
+        getUsers: function() {
+            $.ajax({
+                url: "/data/users.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    this.setUsers(data);
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error("/data/users.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+
+        setUsers: function(data) {
+            this.uids = data.uids;
+        },
+
+        getUser: function(uid) {
+            this.focus = false;
+            $.ajax({
+                url: "/data/alex.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    this.setUser(data);
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    this.focus = true;
+                    console.error("/data/alex.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        setUser: function(data) {
+            this.user = {
+                uid: data.uid,
+                methods: data.user.methods,
+                transports: data.user.transports
+            }
+        }
     },
     template: '#manager-dashboard'
 });
 
+/** Admin **/
 var AdminDashboard = Vue.extend({
     props: {
         'messages': Object,
         'methods': Object
     },
-    template: '#admin-dashboard'
+    template: '#admin-dashboard',
+    methods: {
+        activate: function(event) {
+            this.methods[event.target.name].activate = true;
+            event.target.checked = true;
+            $.ajax({
+                url: "/data/activate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") {
+                        this.methods.activate = false;
+                        event.target.checked = false;
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    this.methods.activate = false;
+                    event.target.checked = false;
+                    console.error("/data/activate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        deactivate: function(event) {
+            console.log("deactivate " + event.target.name);
+            event.target.checked = false;
+            this.methods[event.target.name].activate = false;
+            $.ajax({
+                url: "/data/deactivate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") {
+                        this.methods[event.target.name].activate = true;
+                        event.target.checked = true;
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    this.methods[event.target.name].activate = true;
+                    event.target.checked = true;
+                    console.error("/data/deactivate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        activateTransport: function(method, transport) {
+            event.target.checked = true;
+            $.ajax({
+                url: "/data/activate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") {
+                        event.target.checked = false;
+                    } else {
+                        this.methods[method].transports.push(transport);
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    event.target.checked = false;
+                    console.error("/data/activate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+        deactivateTransport: function(method, transport) {
+            event.target.checked = false;
+            $.ajax({
+                url: "/data/deactivate.json",
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    if (data.code != "Ok") {
+                        event.target.checked = true;
+                    } else {
+                        var index = this.methods[method].transports.indexOf(transport);
+                        if (index > (-1)) this.methods[method].transports.splice(index, 1);
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    event.target.checked = true;
+                    console.error("/data/deactivate.json", status, err.toString());
+                }.bind(this)
+            });
+        },
+    }
 });
 
+/** Main **/
 var app = new Vue({
     el: '#app',
     components: {
@@ -272,31 +596,12 @@ var app = new Vue({
 
         navigate: function(event) {
             if (event.target.name == "manager") {
-                this.getUsers();
                 this.currentView = 'manager-dashboard';
             } else if (event.target.name == "admin") this.currentView = 'admin-dashboard';
             else {
                 this.currentMethod = event.target.name;
                 this.currentView = 'user-dashboard';
             }
-        },
-
-        getUsers: function() {
-            $.ajax({
-                url: "/data/users.json",
-                dataType: 'json',
-                cache: false,
-                success: function(data) {
-                    this.setUsers(data);
-                }.bind(this),
-                error: function(xhr, status, err) {
-                    console.error("/data/users.json", status, err.toString());
-                }.bind(this)
-            });
-        },
-
-        setUsers: function(data) {
-            this.uids = data.uids;
         },
 
         getUser: function() {
